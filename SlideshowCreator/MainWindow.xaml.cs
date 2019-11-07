ae6a624b0361fabe736118b9bcb52a2ab136f313
MarcPekoua <marcpekoua@gmail.com>
2019-11-07 09:09:19 +0100

    Add von "MaterialDesign", Erstellung des Koerpers des Programm

--- SlideshowCreator/MainWindow.xaml.cs
+++ SlideshowCreator/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace SlideshowCreator
         {
             InitializeComponent();
         }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
